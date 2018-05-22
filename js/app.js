e97b5868497d1c346f732e62840a2b33ea49f12d
Geoff <skyward17@outlook.com>
2018-05-22 18:42:39 -0500

    feat: Added Player.prototype.update feat: Added Player.prototype.render

--- js/app.js
+++ js/app.js
@@ -58,6 +58,16 @@ let Player = function() {
 */
 };
 
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function(locX, locY) {
+    ctx.drawImage(Resources.get(this.charboy), this.locX, this.locY);
+};
 // Now instantiate your objects.
 
 // Enemy objects in an array called allEnemies
